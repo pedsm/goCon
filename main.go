e0b9df309ede278be480bce521fb8fce8d4aa41a
Pedro Mendonca <peumendonca@gmail.com>
2017-04-16 10:05:58 +0100

    Sync version

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
+    var buffer int = 0
+    var iterations int = 10
     fmt.Println("Hello World")
+    for i := 0; i < iterations; i++ {
+        buffer = produce(buffer)
+    }
+    for i := 0; i < iterations; i++ {
+        buffer = consume(buffer)
+    }
+}
+
+func produce(buf int) int {
+    buf++
+    fmt.Println(buf)
+    return buf
+}
+
+func consume(buf int) int {
+    buf--
+    fmt.Println(buf)
+    return buf
 }
