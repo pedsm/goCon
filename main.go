0024004b6fe182d3ee437b2921bf385a97b87d96
Pedro Mendonca <peumendonca@gmail.com>
2017-04-16 10:42:57 +0100

    Concurrent(Data races)

--- main.go
+++ main.go
@@ -4,22 +4,33 @@ import "fmt"
 
 //Globals
 var buffer int = 0
-var iterations int = 10
+var iterations int = 10000
+var produceChannel chan int = make(chan int)
+var consumeChannel chan int = make(chan int)
 
 func main() {
-	threadProduce()
-	threadConsume()
+	go threadProduce()
+	go threadConsume()
+	for{
+		pC := <-produceChannel
+		cC := <-consumeChannel
+		if pC != 0 && cC != 0 {
+			break
+		}
+	}
 }
 
 func threadProduce() {
 	for i:= 0; i < iterations; i++ {
 		produce()
 	}
+	produceChannel <- 1
 }
 func threadConsume() {
 	for i:= 0; i < iterations; i++ {
 		consume()
 	}
+	consumeChannel <- 1
 }
 func produce () {
 	buffer++
@@ -27,5 +38,5 @@ func produce () {
 }
 func consume () {
 	buffer--
-	fmt.Println(buffer)
+	fmt.Println(buffer0)
 }
