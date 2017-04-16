c14edf6f9b8d9f7a2ec8250c73d8e758e49dfd8f
Pedro Mendonca <peumendonca@gmail.com>
2017-04-16 10:10:44 +0100

    Added high order function declarations

--- main.go
+++ main.go
@@ -5,23 +5,21 @@ import "fmt"
 func main() {
     var buffer int = 0
     var iterations int = 10
-    fmt.Println("Hello World")
-    for i := 0; i < iterations; i++ {
-        buffer = produce(buffer)
-    }
-    for i := 0; i < iterations; i++ {
-        buffer = consume(buffer)
-    }
+	produce := func (buf int) int {
+		buf++
+		fmt.Println(buf)
+		return buf
+	}
+	consume := func (buf int) int {
+		buf--
+		fmt.Println(buf)
+		return buf
+	}
+	for i := 0; i < iterations; i++ {
+		buffer = produce(buffer)
+	}
+	for i := 0; i < iterations; i++ {
+		buffer = consume(buffer)
+	}
 }
 
-func produce(buf int) int {
-    buf++
-    fmt.Println(buf)
-    return buf
-}
-
-func consume(buf int) int {
-    buf--
-    fmt.Println(buf)
-    return buf
-}
